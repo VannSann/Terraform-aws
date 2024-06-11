936087aa1ce5cc4551d4f60424194fdc3fca5968
VannSann <118683358+VannSann@users.noreply.github.com>
2024-06-11 20:28:38 +0530

    Update variables.tf

--- EC2withJenkins/variables.tf
+++ EC2withJenkins/variables.tf
@@ -5,7 +5,7 @@ variable "region" {
 variable "ami_id" {
   type = "map"
   default = {
-    us-east-1    = "ami-00dc79254d0461090"
+    us-east-1    = "ami-051f8a213df8bc089"
   }
 }
 variable "instance_type" {
