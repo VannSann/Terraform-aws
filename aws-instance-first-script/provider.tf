79545e0c1c776f5bc6ccd92301d6fd082952179d
VannSann <118683358+VannSann@users.noreply.github.com>
2024-06-11 21:59:33 +0530

    Update provider.tf

--- aws-instance-first-script/provider.tf
+++ aws-instance-first-script/provider.tf
@@ -1,4 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
-  region  = "${var.region}"
-  version = "~>5.0"
+  region = "us-west-2"
 }
